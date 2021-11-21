6c2a30ff3680b972e5de6a2b90f905c911e61c92
Rajendra152 <rajendraprasadbhukya152927@gmail.com>
2021-11-21 11:57:57 +0530

    Done with the Routing of the Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PasswordComponent } from './password/password.component';
+import { LoginComponent } from './login/login.component';
+import { LandingComponent } from './landing/landing.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'',component:LandingComponent},
+  {path:'login',component:LoginComponent},
+  {path:'signup',component:SignupComponent},
+  {path:'password',component:PasswordComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
