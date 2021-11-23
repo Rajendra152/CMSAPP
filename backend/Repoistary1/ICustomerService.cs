ef341782943a89ccc605db44514c62775fc2237d
Sachinkoppad <sachinkoppad111@gmail.com>
2021-11-23 18:58:59 +0530

    created backend

---
+++ backend/Repoistary1/ICustomerService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using CustomerDetails;
+
+
+namespace Repoistary1
+{
+    public interface ICustomerService
+    {
+        IEnumerable<Customer> GetCustomers();
+        bool UpdateContent(string pass,string email);
+        bool Insertdata(Customer customer);
+        bool DeleteContent(string email);
+         bool login(string email,string pass);
+        string Getcontentbyid(string email);
+    }
+}
