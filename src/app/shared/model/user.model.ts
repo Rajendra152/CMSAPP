f9118fa0c4a5dc921a55334d47675d77d1530f5c
Rajendra152 <rajendraprasadbhukya152927@gmail.com>
2021-11-29 10:54:12 +0530

    made changes

---
+++ src/app/shared/model/user.model.ts
@@ -0,0 +1,12 @@
+export class UserModel{
+    UserName!:string;
+    Emailid!: string;
+    phnNo!:string;
+    CreatePassword!:string;
+    ConfirmPassword!:string;
+    Name!:string;
+    Gender!:string;
+    Age!:string;
+    Nationality!:string
+
+}
